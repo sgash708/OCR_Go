55b0e2ffabee930613697567ba549558d0049757
sgash708 <sugash7081023@gmail.com>
2021-09-07 17:54:46 +0900

    Dockerfileのリファクタリング

--- server/main.go
+++ server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/otiai10/gosseract/v2"
 )
@@ -11,11 +12,11 @@ func main() {
 	client := gosseract.NewClient()
 	defer client.Close()
 
-	client.Languages = []string{"jpn"}
+	client.Languages = []string{os.Getenv("LANG")}
 	client.SetImage("test/data/kanji/o.png")
 	text, err := client.Text()
 	if err != nil {
-		log.Fatalln("文字を認識できませんでした。")
+		log.Fatalln("テキストに変換できませんでした。")
 	}
 	fmt.Println(text)
 }
