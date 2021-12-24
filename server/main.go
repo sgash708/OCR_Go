dc85669cbd8f7595839edf957d1648649fd008c0
sgash708 <sugash7081023@gmail.com>
2021-12-25 00:39:33 +0900

    ファイルの実験

--- server/main.go
+++ server/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/otiai10/gosseract/v2"
@@ -14,10 +16,42 @@ func main() {
 
 	client.Languages = []string{os.Getenv("LANG")}
 	// TODO:DIテスト
-	client.SetImage("data/kanji/saki.png")
+
+	fileName := "./img/E054.png"
+	if err := DownloadFile(fileName, "https://www.nichibenren.jp/member_general/font/E1BF.png"); err != nil {
+		panic(err)
+	}
+	client.SetImage(fileName)
 	text, err := client.Text()
 	if err != nil {
 		log.Fatalln("テキストに変換できませんでした。")
 	}
 	fmt.Println(text)
+}
+
+// DownloadFile ファイルダウンロード
+func DownloadFile(filepath string, url string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err := resp.Body.Close(); err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err := out.Close(); err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
+	_, err = io.Copy(out, resp.Body)
+
+	return err
 }
